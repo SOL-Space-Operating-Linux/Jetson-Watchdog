3063074288ee5cd94b7d221dfc2103d4e79f2623
Amanda Voegtlin <amanda.voegtlin@jhuapl.edu>
2020-05-28 11:34:12 -0400

    Added graceful trap on CTRL-C exit to script, added more errors in the mix

--- src/dmesg_bash_script.sh
+++ src/dmesg_bash_script.sh
@@ -4,6 +4,14 @@
 # This script is to simulate radiation damage to a board. It will randomly pick an error 
 # and then pick a random number of times to repeat it, spaced out in random milliseconds
 # and output that to dmesg on a constant loop until killed. Must be run in sudo.
+
+do_on_exit(){
+    echo 'NOTIFICATION: dmesg_bash_script.sh stopped by CTRL-C signal' > /dev/kmsg
+    exit 0
+}
+
+trap 'do_on_exit' SIGINT
+
 arr[0]="SBE ERR"
 arr[1]="Serror Detected"
 arr[2]="CPU Memory Error"
@@ -16,13 +24,22 @@ arr[8]="watchdog detected"
 
 while true; do 
     error=$(($RANDOM % 9))
-    repeat=$(($RANDOM % 5))
-    for ((i=0; i <= $repeat; i++)) 
+    repeaterror=$(($RANDOM % 5))
+    for ((i=0; i <= $repeaterror; i++)) 
         do 
             echo ${arr[$error]} > /dev/kmsg
-            # echo ${arr[$error]}
+            extraerror=$(($RANDOM % 5)) # Sometimes, we get more errors! 
+            if (($extraerror <= 1)); then # 40% of the time
+                echo ${arr[$RANDOM % 9]} > /dev/kmsg
+            fi
+            if (($extraerror == 2)); then # 20% of the time
+                echo ${arr[$RANDOM % 9]} > /dev/kmsg
+                echo ${arr[$RANDOM % 9]} > /dev/kmsg
+                echo ${arr[$RANDOM % 9]} > /dev/kmsg
+                echo ${arr[$RANDOM % 9]} > /dev/kmsg
+            fi
             sleep .000$[($RANDOM % 10) + 1]s 
-            repeat=$repeat-1
+            repeaterror=$repeat-1
         done
-    sleep $[($RANDOM % 5) + 1]s
+    sleep $[($RANDOM % 5) + 1]s # wait a few seconds before the next radiation surge hits 
 done

f1262e3734aee6a752be2bfc159dc9842980fc36
Amanda Voegtlin <amanda.voegtlin@jhuapl.edu>
2020-06-15 16:47:19 -0400

    Cleaned up commented out code, added description

--- src/log_daemon.rs
+++ src/log_daemon.rs
@@ -1,12 +1,20 @@
+/* This program spawns a child process that reads from dmesg and puts dmesg lines into a buffer. 
+The main thread then reads that buffer and collects timestamps for each of 9 error types into vectors of timestamps.
+Child process kill command is UNIX only, no Windows implementation. 
+
+A Voegtlin, SIE-3
+JHU APL 6/2020
+Supercomputing in Space IRAD
+*/
+
 extern crate logwatcher;
 extern crate regex;
 // use logwatcher::LogWatcher;
 use regex::Regex;
 //for preexisting logs
-use std::env;
-use std::fs; //accessing files
-use std::vec; //vectors
-use crate::public_logwatcher:: LogWatcher; // neighboring *.rs file
+// use std::env;
+// use std::fs; //accessing files
+// use std::vec; //vectors
 // -----------
 use std::process::{Command, Stdio};
 use std::io::{BufRead, BufReader, Error, ErrorKind};
@@ -26,31 +34,23 @@ pub fn main () {
     let mut watchdog_detected_vec: Vec<f32> = Vec::new(); 
     // Create the regex
     let re = Regex::new(r"(\[.?[0-9]+\.[0-9]+\])\s(SBE ERR|SError detected|CPU Memory Error|Machine Check Error|GPU L2|generated a mmu fault|SDHCI_INT_DATA_TIMEOUT|Timeout waiting for hardware interrupt|watchdog detected)").unwrap();
+    // Create the child process, which watches dmesg outputs change 
     let mut child = Command::new("dmesg")
         .arg("-w")
         .stdout(Stdio::piped())
         .spawn()
         .expect("Unable to spawn dmesg child program");
-
-    while let Some(ref mut stdout) = child.stdout {
-        let lines = BufReader::new(stdout).lines();//.enumerate().take(1);
+// MAIN LOOP: read the child's stdout buffer forever, and process. 
+// TODO: If child dies, relaunch
+// TODO: Output to JSON
+    while let Some(ref mut stdout) = child.stdout { // while there is something in child's stdout pipe
+        let lines = BufReader::new(stdout).lines();
         for line in lines { 
-            // for cap in re.captures_iter(&line) {
-            let mut our_string = line.unwrap().to_string();
-            // DEBUG PRINT STATEMENTS
-            // println!("{:?}", line);
-            // println!("{}", &our_string);
-
+            let mut our_string = line.unwrap().to_string(); // string type so we can run it through regex
             for cap in re.captures_iter(&our_string) {
-                // DEBUG PRINTS
-                // println!("Found: {}", &cap[0]);   // whole expression from timestamp to error regex
-                // println!("Timestamp: {}", &cap[1]); // timestamp only
-                // println!("Error: {}\n", &cap[2]); // error only
-            
-                let error_type = cap.get(2).unwrap().as_str();
-                let raw_timestamp = cap.get(1).unwrap().as_str().replace("[", "").replace("]", "").replace(" ", "");
+                let error_type = cap.get(2).unwrap().as_str(); // take the second argument of the regex, which is the error message
+                let raw_timestamp = cap.get(1).unwrap().as_str().replace("[", "").replace("]", "").replace(" ", ""); // take the timestamp
                 let timestamp = raw_timestamp.parse::<f32>().unwrap(); // FIXME: can we process this as a string?
-                // println!("Raw timestamp: {}", raw_timestamp);
                 // save the timestamp on the global errors vector, then according the individual error type
                 all_errors_vec.push(timestamp);
                 match error_type { // switch-case statement for processing each error
@@ -77,32 +77,8 @@ pub fn main () {
                 println!("Watchdog CPU Error total (detected): {}", watchdog_detected_vec.len());
                 println!("All errors: {}", all_errors_vec.len());
             }
-            continue;
-        }
-    }
-    
-
-    // update_vector(&mut all_errors_vec, timestamp);
-
-    // // println!("Length of sbe_err_vector: {}", sbe_err_vec.len());
-    // // sbe_err_vec.sort_by(|a,b| b.partial_cmp(a).unwrap()); // reverse the timestamps to make life easier 
-    // let mut sbe_iter = sbe_err_vec.iter().enumerate(); // do I need an iterator object?
-
-    // // Display error totals to stdout
-    // println!("SBE ERR total: {}", sbe_err_vec.len());
-    // println!("Serror total: {}", serror_vec.len());
-    // println!("CPU Memory Error total: {}", cpu_mem_vec.len());
-    // println!("CCE Machine Check Error total: {}", cce_machine_vec.len());
-    // println!("GPU L2 Error total: {}", gpu_l2_vec.len());
-    // println!("MMU Fault Error Counter: {}", mmu_fault_vec.len());
-    // println!("Flash Write Error total: {}", flash_write_vec.len());
-    // println!("Flash Read Error total: {}",flash_read_vec.len());
-    // println!("Watchdog CPU Error total (detected): {}", watchdog_detected_vec.len());
-    // println!("All errors: {}", all_errors_vec.len());
+            continue;        
+        } // end of line processing
+    } // end of while
+    child.wait().expect("log_daemon.rs: Failed to wait on child");
 }
-
-// pub fn update_vector (vector: &mut Vec<f32>, timestamp: f32) -> bool {
-
-//     vector.push(timestamp);
-//     return true;
-// }

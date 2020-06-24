0705b4e24c247bc01b1211c49141cd24f23e8857
Amanda Voegtlin <amanda.voegtlin@jhuapl.edu>
2020-06-24 14:08:35 -0400

    log_daemon now detects when the dmesg process has died and closes its loop.

--- src/log_daemon.rs
+++ src/log_daemon.rs
@@ -16,24 +16,10 @@ use std::fs; //accessing files
 // -----------
 use std::process::{Command, Stdio};
 use std::io::{BufRead, BufReader};
-use chrono::{Datelike, Timelike, Utc};
+use std::sync::Arc;
+use shared_child::SharedChild; 
 
 pub fn main () {
-    // Generate, format and assemble the current date-time string in UTC
-    let now = Utc::now();
-    let (is_pm, hour) = now.hour12();
-    let my_timestamp = 
-        format!("{:0>2}:{:0>2}:{:0>2} {} {}-{}-{:0>2}", 
-            hour.to_string(),
-            now.minute().to_string(), 
-            now.second().to_string(),
-            (if is_pm { "PM" } else { "AM" }),
-            year.to_string(),
-            now.month().to_string(),
-            now.day().to_string());
-    // Open a log file with that name
-    //  FIXME: add a daily check to make a new one every day?
-
     // Create empty, dynamic vectors to store error timestamps in for later processing.
     //     FIXME: Vectors cannot be left to grow without bounds. Create a fixed length and a way of saving results if the length is exceeded.
     let mut all_errors_vec: Vec<f32> = Vec::new();
@@ -49,15 +35,14 @@ pub fn main () {
     // Create the regex
     let re = Regex::new(r"(\[.?[0-9]+\.[0-9]+\])(.*?)(SBE ERR|SError detected|CPU Memory Error|Machine Check Error|GPU L2|generated a mmu fault|SDHCI_INT_DATA_TIMEOUT|Timeout waiting for hardware interrupt|watchdog detected)").unwrap();
     // Create the child process, which watches dmesg outputs change 
-    let mut child = Command::new("dmesg")
+    let mut dmesg_child = Command::new("dmesg")
         .arg("-w")
         .stdout(Stdio::piped())
         .spawn()
         .expect("Unable to spawn dmesg child program");
-// MAIN LOOP: read the child's stdout buffer forever, and process. 
-// TODO: If child dies, relaunch
-// TODO: Output to JSON
-    while let Some(ref mut stdout) = child.stdout { // while there is something in child's stdout pipe
+
+    // MAIN LOOP: read the child's stdout buffer forever, and process. 
+    while let Some(ref mut stdout) = dmesg_child.stdout { // while there is something in child's stdout pipe
         let lines = BufReader::new(stdout).lines();
         for line in lines { 
             let our_string = line.unwrap().to_string(); // string type so we can run it through regex
@@ -65,7 +50,7 @@ pub fn main () {
                 // println!("{}", cap.get(1).unwrap().as_str());
                 // println!("{}", cap.get(2).unwrap().as_str());
                 // println!("{}", cap.get(3).unwrap().as_str());
-                let error_type = cap.get(3).unwrap().as_str(); // take the second argument of the regex, which is the error message
+                let error_type = cap.get(3).unwrap().as_str(); // take the third argument of the regex, which is the error message
                 let raw_timestamp = cap.get(1).unwrap().as_str().replace("[", "").replace("]", "").replace(" ", ""); // take the timestamp
                 let timestamp = raw_timestamp.parse::<f32>().unwrap(); // FIXME: can we process this as a string?
                 // save the timestamp on the global errors vector, then according the individual error type
@@ -93,9 +78,19 @@ pub fn main () {
                 println!("Flash Read Error total: {}",flash_read_vec.len());
                 println!("Watchdog CPU Error total (detected): {}", watchdog_detected_vec.len());
                 println!("All errors: {}", all_errors_vec.len());
-            }
-            continue;        
-        } // end of line processing
+                // println!("Process ID is: {}", dmesg_child.id());
+            }       
+        } // end of line processing, this is an infinite loop
+        // If child dies, kill this loop
+// TODO: Shutdown and return a signal to main to relaunch this! 
+        match dmesg_child.try_wait() {
+            Ok(Some(status)) => {println!("exited with: {}", status); break;},
+            Ok(None) => break,
+            Err(e) => {println!("error attempting to wait: {}", e); return;},
+        }
     } // end of while
-    child.wait().expect("log_daemon.rs: Failed to wait on child");
+
+    println!("Dmesg process exited!");
+
+    return;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+
 extern crate difference;
 mod watchdog_daemon;
 mod log_daemon; // Our implementation of a dmesg watcher
+use chrono::{Datelike, Timelike, Utc};
 
 // USAGE: sudo ~/jetson-watchdog
 
@@ -8,7 +10,39 @@ mod log_daemon; // Our implementation of a dmesg watcher
 
 fn main() {
 
+        // Generate, format and assemble the current date string in UTC
+        let now = Utc::now();
 
+        let (is_common_era, year) = now.year_ce();
+        let my_timestamp = 
+            format!("{}-{}-{:0>2}", 
+                year.to_string(),
+                now.month().to_string(),
+                now.day().to_string());
+        //for a timestamp that also includes time of open and AM/PM, use code below 
+/*                    
+        let (is_pm, hour) = now.hour12();
+        let my_timestamp = 
+            format!("{:0>2}:{:0>2}:{:0>2} {} {}-{}-{:0>2}", 
+                hour.to_string(),
+                now.minute().to_string(), 
+                now.second().to_string(),
+                (if is_pm { "PM" } else { "AM" }),
+                year.to_string(),
+                now.month().to_string(),
+                now.day().to_string()); */
+// TODO: Output to JSON
+        // Open a log file with that name
+        // let contents = fs::read_to_string(filename)
+        //     .expect("Something went wrong reading the log");
+        //  FIXME: add a daily check to make a new one every day?
+        //  - use same one each day, have Main bark to dmesg to say it rebooted (should be obvious)
+
+        // Discard oldest log after thirty days
+        //  -best way is with linux command: 'find /var/log -name "*.json" -type f -mtime +30 -exec rm -f {} \;'
+
+
+        
     //load config, use flight defaults if no file, or parts missing
 
     //wait for boot to finish

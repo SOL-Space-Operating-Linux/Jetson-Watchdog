4abe3e9a978f8fa32a46a7a962f18c62ff97c4f2
Amanda Voegtlin <amanda.voegtlin@jhuapl.edu>
2020-07-08 10:26:04 -0400

    Constructs a json from the first and last values of error messages, detects empty vectors and empty files

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,17 @@ extern crate difference;
 extern crate crossbeam_channel;
 extern crate regex;
 extern crate serde_json;
+extern crate serde;
+extern crate serde_derive;
 mod watchdog_daemon;
 mod log_daemon; // Our implementation of a dmesg watcher
-use serde_json::json;
+use serde_json::{json,Value, Error};
+use serde::{Serialize, Deserialize};
 
 use regex::Regex;
 use crossbeam_channel::unbounded;
 use chrono::{Datelike, Timelike, Utc};
-use std::{thread, time};
+use std::{thread, time, fs, io::Read};
 use std::process::{Command, Stdio};
 // use nix::sys::wait::waitpid;
 // use nix::unistd::{fork, getpid, getppid, ForkResult};
@@ -20,19 +23,19 @@ use std::process::{Command, Stdio};
 // USAGE: sudo ~/jetson-watchdog
 
 //global check register
-
-// struct Error_Timestamps { // internal components of struct are private even if struct is public 
-//     pub all_errors_vec: Vec<f32>,
-//     pub sbe_err_vec: Vec<f32>,
-//     pub serror_vec: Vec<f32>,
-//     pub cpu_mem_vec: Vec<f32>, 
-//     pub cce_machine_vec: Vec<f32>, 
-//     pub gpu_l2_vec: Vec<f32>,
-//     pub mmu_fault_vec: Vec<f32>, 
-//     pub flash_write_vec: Vec<f32>, 
-//     pub flash_read_vec: Vec<f32>,
-//     pub watchdog_detected_vec: Vec<f32> 
-// }
+#[derive(Deserialize, Debug)]
+struct Error_Timestamps { // internal components of struct are private even if struct is public 
+    pub all_errors_vec: Vec<f32>,
+    pub sbe_err_vec: Vec<f32>,
+    pub serror_vec: Vec<f32>,
+    pub cpu_mem_vec: Vec<f32>, 
+    pub cce_machine_vec: Vec<f32>, 
+    pub gpu_l2_vec: Vec<f32>,
+    pub mmu_fault_vec: Vec<f32>, 
+    pub flash_write_vec: Vec<f32>, 
+    pub flash_read_vec: Vec<f32>,
+    pub watchdog_detected_vec: Vec<f32> 
+}
 
 // impl Error_Timestamps {
 //     pub fn new() -> Error_Timestamps {
@@ -40,46 +43,67 @@ use std::process::{Command, Stdio};
 //     }
 // }
 
+
+
+/*------------------ HELPER FUNCTIONS --------------------*/
 /*Function that returns the first element of a vector without destroying it*/
 fn first<T>(v: &Vec<T>) -> Option<&T> {
-    v.first()
+    if !v.is_empty() {
+        serde::export::Some(v.first().unwrap())
+    }
+    else {
+        return None
+    }
 }
+/*Function that returns the last element of a vector without destroying it*/
 fn last<T>(v: &Vec<T>) -> Option<&T> {
-    v.last()
+    if !v.is_empty() {
+        serde::export::Some(v.last().unwrap())
+    }
+    else {
+        return None
+    }
+
+}
+/* Function that merges two structs, with the second struct overriding the first if there's a conflict */
+fn merge(a: &mut Value, b: Value) {
+    match (a, b) {
+        (a @ &mut Value::Object(_), Value::Object(b)) => {
+            let a = a.as_object_mut().unwrap();
+            for (k, v) in b {
+                merge(a.entry(k).or_insert(Value::Null), v);
+            }
+        }
+        (a, b) => *a = b,
+    }
 }
 /*Function to be called on a thread that sleeps for one second and then sends a message to the receiver */
 fn taskmaster_thread(sender: crossbeam_channel::Sender<String>) {
     let message = "Timer interrupt";
-    let one_second = time::Duration::from_millis(1000);
+    let one_second = time::Duration::from_secs(1);
     loop {
         thread::sleep(one_second);
         sender.try_send(message.to_string()).unwrap();
     }
 }
 
+/*---------------MAIN--------------------*/
 fn main() {
-
+    //TODO: Put this in its own function
         // Generate, format and assemble the current date string in UTC
         let now = Utc::now();
         let (is_common_era, year) = now.year_ce();
-        let my_timestamp = 
+        let todays_date = 
             format!("{}-{}-{:0>2}", 
                 year.to_string(),
                 now.month().to_string(),
                 now.day().to_string());
-        //for a timestamp that also includes time of open and AM/PM, use code below 
-/*                    
-        let (is_pm, hour) = now.hour12();
-        let my_timestamp = 
-            format!("{:0>2}:{:0>2}:{:0>2} {} {}-{}-{:0>2}", 
-                hour.to_string(),
-                now.minute().to_string(), 
-                now.second().to_string(),
-                (if is_pm { "PM" } else { "AM" }),
-                year.to_string(),
-                now.month().to_string(),
-                now.day().to_string()); */
-
+        // open today's file, creating it if it doesn't exist
+        let file = fs::OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(true)
+                .open(todays_date);
 // TODO: Output to JSON
         // Open a log file with that name
         // let contents = fs::read_to_string(filename)
@@ -89,6 +113,9 @@ fn main() {
 
         // Discard oldest log after thirty days
         //  -best way is with linux command: 'find /var/log -name "*.json" -type f -mtime +30 -exec rm -f {} \;'
+        // but there is also a way to do this with chrono library's signed_duration_since command, where we can specify older than 30 days
+        // and use std::fs::read_dir to list all the files in the output log's directory 
+        // see https://doc.rust-lang.org/std/fs/fn.read_dir.html
 
 
 
@@ -106,6 +133,9 @@ fn main() {
     //update new baseline, # resets
 
 /* -----------------------------------------LOG DAEMON WORK AREA----------------------------------------------*/
+    // Create the regex for local parse and sort use 
+    let re = Regex::new(r"(\[.?[0-9]+\.[0-9]+\])(.*?)(SBE ERR|SError detected|CPU Memory Error|Machine Check Error|GPU L2|generated a mmu fault|SDHCI_INT_DATA_TIMEOUT|Timeout waiting for hardware interrupt|watchdog detected)").unwrap();
+    // Create the vectors to hold timestamps
     let mut all_errors_vec: Vec<f32> = Vec::new();
     let mut sbe_err_vec: Vec<f32> = Vec::new();
     let mut serror_vec: Vec<f32> = Vec::new();
@@ -120,8 +150,8 @@ fn main() {
     let (s, receiver) = unbounded();
 
 /* This portion of the program creates the dmesg -w thread and then spawns a process, log_daemon, which watches that thread*/
-
-    // Set up thread builder; this will let us capture an io::Result if the OS fails to create it
+// TODO: This needs to return some value to show that it, or dmesg, died.     
+// Set up thread builder; this will let us capture an io::Result if the OS fails to create it
     let dmesg_builder = thread::Builder::new();
     let sender = s.clone(); // clone the sender to send off to the thread
         // TODO: capture OS failure to create the thread 
@@ -139,6 +169,7 @@ fn main() {
     let mut logdaemon_handle = dmesg_builder.spawn(move || {
         log_daemon::startup(sender, &mut dmesg_child); // a separate thread launches this. 
     }).unwrap();
+/*-------------End log_daemon section-----------*/
 
 // Create a thread which serves as the heartbeat, sleeping and then periodically sending a message
     let interrupt_builder = thread::Builder::new();
@@ -146,28 +177,68 @@ fn main() {
     let taskmaster_timer = interrupt_builder.spawn(move || {
         taskmaster_thread(interrupt_sender);
     }).unwrap();
-
-    // Create the regex for local parse and sort use 
-    let re = Regex::new(r"(\[.?[0-9]+\.[0-9]+\])(.*?)(SBE ERR|SError detected|CPU Memory Error|Machine Check Error|GPU L2|generated a mmu fault|SDHCI_INT_DATA_TIMEOUT|Timeout waiting for hardware interrupt|watchdog detected)").unwrap();
-
-    // Launch the taskmaster thread, which will tell our main thread when it should analyze data and put it away
-
     // TODO: This needs to return some value to show that it, or dmesg, died. 
+    let sys_time = time::SystemTime::now();
     loop { 
         let our_string = receiver.recv().unwrap().to_string(); // this blocks until there is a message on the channel.
         // println!("Main received: {}", our_string);
-
         if our_string.eq("Timer interrupt") {
             println!("Time interrupt detected!");
+            println!("First item in watchdog queue: {}", first(&watchdog_detected_vec).unwrap());
+            println!("Last item in watchdog queue: {}", last(&watchdog_detected_vec).unwrap() );
+
+            println!("Time elapsed: {:?}", sys_time.elapsed().unwrap());
+            // create a json object using the first and last contents of all of these vectors
+            let new_json = json!({
+                "SBE ERR": [first(&sbe_err_vec), last(&sbe_err_vec)],
+                "SError detected" : [first(&serror_vec), last(&serror_vec)],
+                "CPU Memory Error": [first(&cpu_mem_vec), last(&cpu_mem_vec)],
+                "Machine Check Error": [first(&cce_machine_vec), last(&cce_machine_vec)],
+                "GPU L2": [first(&gpu_l2_vec), last(&gpu_l2_vec)],
+                "generated a mmu fault": [first(&mmu_fault_vec), last(&mmu_fault_vec)],
+                "SDHCI_INT_DATA_TIMEOUT": [first(&flash_write_vec), last(&flash_write_vec)],
+                "Timeout waiting for hardware interrupt": [first(&flash_read_vec), last(&flash_read_vec)],
+                "watchdog detected":[first(&watchdog_detected_vec)., last(&watchdog_detected_vec)]
+            });
+            println!("{:#}", new_json);
+            //load the previous json object from a file, if it exists
+            let mut file = fs::OpenOptions::new()
+                .create(true)
+                .write(true)
+                .read(true)
+                .open("testfile.txt")
+                .expect("Unable to open file");
+
+            let mut file_as_string = String::new();
+            file.read_to_string(&mut file_as_string).unwrap();
+            // If this throws an error, it's because there is nothing in the file
+
+            if file_as_string.is_empty() {
+                println!("Empty file!");
+
+            } 
+            else{ 
+                let mut file_json = serde_json::from_str(&file_as_string).unwrap(); 
+                println!("JSON in the file was {:#?}", file_json);
+                merge(&mut file_json, new_json);
+            }
+
+            // call merge() on the two json objects, with the file_json being the first argument, and the new json being the second
+
+            // merge(&mut file_json, new_json);
+            // write out to the file-- destroy the old contents?
+            // clear the vectors
+            // somevector.clear();
+            //close the file
+            // file.close(); ???
+
         }
         else if !our_string.eq("Lost dmesg process") && !our_string.eq("Timer interrupt"){ 
             for cap in re.captures_iter(&our_string) {
-                // println!("{}", cap.get(1).unwrap().as_str());
-                // println!("{}", cap.get(2).unwrap().as_str());
-                // println!("{}", cap.get(3).unwrap().as_str());
                 let error_type = cap.get(3).unwrap().as_str(); // take the third argument of the regex, which is the error message
                 let raw_timestamp = cap.get(1).unwrap().as_str().replace("[", "").replace("]", "").replace(" ", ""); // take the timestamp
                 let timestamp = raw_timestamp.parse::<f32>().unwrap(); // FIXME: can we process this as a string?
+
                 // save the timestamp on the global errors vector, then according the individual error type
                 all_errors_vec.push(timestamp);
                 match error_type { // switch-case statement for processing each error
@@ -194,13 +265,8 @@ fn main() {
                 println!("Watchdog CPU Error total (detected): {}", watchdog_detected_vec.len());
                 println!("All errors: {}", all_errors_vec.len());
                 // println!("{}", serde_json::encode(&watchdog_detected_vec));
-
-
-
-            // When the program times out
-
-            println!("First item in watchdog queue: {}", first(&watchdog_detected_vec).unwrap());
-            println!("Last item in watchdog queue: {}", last(&watchdog_detected_vec).unwrap() );
+                println!("First item in watchdog queue: {}", first(&watchdog_detected_vec).unwrap());
+                println!("Last item in watchdog queue: {}", last(&watchdog_detected_vec).unwrap());
             }
     
         }
